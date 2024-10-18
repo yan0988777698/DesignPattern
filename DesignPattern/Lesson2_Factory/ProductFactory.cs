68382a6beb1a71e0e5bc9b66dd0d1554e9171bba
彥霖 陳 <yan0988777698@gmail.com>
2024-10-18 20:43:30 +0800

    Factory

---
+++ DesignPattern/Lesson2_Factory/ProductFactory.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPattern.Lesson2_Factory
+{
+    public interface IProduct
+    {
+        void ChangeColor(string color);
+    }
+    public class Bike : IProduct
+    {
+        private string color = "black";
+        public Bike()
+        {
+            
+        }
+        public void ChangeColor(string color)
+        {
+            this.color = color;
+        }
+        public override string ToString()
+        {
+            return $"This bike is {color} color.";
+        }
+    }
+    public class Car : IProduct
+    {
+        private string color = "black";
+        public Car()
+        {
+
+        }
+        public void ChangeColor(string color)
+        {
+            this.color = color;
+        }
+        public override string ToString()
+        {
+            return $"This car is {color} color.";
+        }
+    }
+    public static class ProductFactory
+    {
+        public static IProduct Create(string product)
+        {
+            switch (product)
+            {
+                case "Car":
+                    return new Car();
+                case "Bike":
+                    return new Bike();
+                default:
+                    throw new Exception("Error product name.");
+            }
+
+        }
+        public static void Demo()
+        {
+            IProduct car = ProductFactory.Create("Car");
+            Console.WriteLine(car);
+            car.ChangeColor("red");
+            Console.WriteLine(car);
+            Console.ReadLine();
+        }
+    }
+}
